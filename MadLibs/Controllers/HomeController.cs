24c9a770063efe6eaa1904aefec89b544cba8338
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-18 15:11:45 -0600

    begin modularization

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using MadLibs.Models;
+using System.Collections.Generic;
 
 namespace MadLibs.Controllers
 {
@@ -12,29 +13,32 @@ namespace MadLibs.Controllers
     [Route("/madlibone")]
     public ActionResult MadLibOne(string name, string sillyword, string number, string adjectiveOne, string adjectiveTwo, string adjectiveThree, string adjectiveFour, string adjectiveFive, string noun, string verb, string relative)
     {
-      MadLibInputs tempLib = new MadLibInputs();
-      tempLib.WordOne = name;
-      tempLib.WordTwo = sillyword;
-      tempLib.WordThree = number;
-      tempLib.WordFour = adjectiveOne;
-      tempLib.WordFive = noun;
-      tempLib.WordSix = adjectiveTwo;
-      tempLib.WordSeven = relative;
-      tempLib.WordEight = adjectiveThree;
-      tempLib.WordNine = verb;
-      tempLib.WordTen = adjectiveFour;
-      tempLib.WordEleven = adjectiveFive;
+      // MadLibInputs tempLib = new MadLibInputs();
+      // tempLib.WordOne = name;
+      // tempLib.WordTwo = sillyword;
+      // tempLib.WordThree = number;
+      // tempLib.WordFour = adjectiveOne;
+      // tempLib.WordFive = noun;
+      // tempLib.WordSix = adjectiveTwo;
+      // tempLib.WordSeven = relative;
+      // tempLib.WordEight = adjectiveThree;
+      // tempLib.WordNine = verb;
+      // tempLib.WordTen = adjectiveFour;
+      // tempLib.WordEleven = adjectiveFive;
+
+      Dictionary<int, string> madLibInput = new Dictionary<int, string> { { 1, name }, { 2, sillyword }, { 3, number }, { 4, adjectiveOne }, { 5, noun }, { 6, adjectiveTwo }, { 7, relative }, { 8, adjectiveThree }, { 9, verb }, { 10, adjectiveFour }, { 11, adjectiveFive } };
+
+      MadLibInputs tempLib = new MadLibInputs(madLibInput);
 
       return View(tempLib);
     }
 
-    [Route("/madlibtwo")]
+    // [Route("/madlibtwo")]
 
-    public ActionResult MadLibTwo(string name, string unrealisticprofession, string country, string sillyword, string color, string adjective, string adverb, string sillynameone, string sillynametwo, string facialfeature, string uscity, string sillynamethree, string verb, string nounone, string formerlybadassactornowsellingout, string nountwo)
-    {
-      MadLibInputs tempLibTwo = new MadLibInputs();
-    }
-
+    // public ActionResult MadLibTwo(string name, string unrealisticprofession, string country, string sillyword, string color, string adjective, string adverb, string sillynameone, string sillynametwo, string facialfeature, string uscity, string sillynamethree, string verb, string nounone, string formerlybadassactornowsellingout, string nountwo)
+    // {
+    //   MadLibInputs tempLibTwo = new MadLibInputs();
+    // }
 
 
   }
--- MadLibs/Models/MadLibInputs.cs
+++ MadLibs/Models/MadLibInputs.cs
@@ -1,18 +1,15 @@
+using System.Collections.Generic;
 namespace MadLibs.Models
 {
   public class MadLibInputs
   {
-    public string Name { get; set; }
-    public string SillyWord { get; set; }
-    public string Number { get; set; }
-    public string AdjectiveOne { get; set; }
-    public string Noun { get; set; }
-    public string AdjectiveTwo { get; set; }
-    public string Relative { get; set; }
-    public string AdjectiveThree { get; set; }
-    public string Verb { get; set; }
-    public string AdjectiveFour { get; set; }
-    public string AdjectiveFive { get; set; }
+    public Dictionary<int, string> WordListDictionary { get; set; }
+
+
+    public MadLibInputs(Dictionary<int, string> userInput)
+    {
+      WordListDictionary = userInput;
+    }
 
   }
 }
