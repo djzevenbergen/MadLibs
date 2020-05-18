a9a50948b226ec7d4014b299b3652316b36ac603
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-18 12:23:22 -0600

    add controllers and populate object

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -6,5 +6,26 @@ namespace MadLibs.Controllers
   public class HomeController : Controller
   {
 
+    [Route("/")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/madlibone")]
+    public ActionResult MadLibOne(string name, string sillyword, string number, string adjectiveOne, string adjectiveTwo, string adjectiveThree, string adjectiveFour, string adjectiveFive, string noun, string verb, string relative)
+    {
+      MadLibInputs tempLib = new MadLibInputs();
+      tempLib.Name = name;
+      tempLib.SillyWord = sillyword;
+      tempLib.Number = number;
+      tempLib.Noun = noun;
+      tempLib.Verb = verb;
+      tempLib.Relative = relative;
+      tempLib.AdjectiveOne = adjectiveOne;
+      tempLib.AdjectiveTwo = adjectiveTwo;
+      tempLib.AdjectiveThree = adjectiveThree;
+      tempLip.AdjectiveFour = adjectiveFour;
+      tempLib.AdjectiveFive = adjectiveFive;
+
+      return View(tempLib);
+    }
   }
 }
---
+++ MadLibs/Models/MadLibInputs.cs
@@ -0,0 +1,18 @@
+namespace MadLibs.Models
+{
+  public class MadLibInputs
+  {
+    public string Name { get; set; }
+    public string SillyWord { get; set; }
+    public string Number { get; set; }
+    public string AdjectiveOne { get; set; }
+    public string Noun { get; set; }
+    public string AdjectiveTwo { get; set; }
+    public string Relative { get; set; }
+    public string AdjectiveThree { get; set; }
+    public string Verb { get; set; }
+    public string AdjectiveFour { get; set; }
+    public string AdjectiveFive { get; set; }
+
+  }
+}
